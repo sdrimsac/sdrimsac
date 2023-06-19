5acf9a088e124103ef67d6705c4a17e42182df7e
Darkent <darkent4ever@gmail.com>
2023-06-19 12:12:49 -0500

    seeder to migrations

--- database/migrations/tenant/2023_06_19_191345_insert_data_seeder.php
+++ database/migrations/tenant/2023_06_19_191345_insert_data_seeder.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
 
-class AddUserToCategories extends Migration
+class InsertDataSeeder extends Migration
 {
 
     public function up()
