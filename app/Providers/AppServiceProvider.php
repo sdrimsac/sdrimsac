b76c466bf38e8edf69bf761e00bba78ff0b5fd75
Darkent <darkent4ever@gmail.com>
2023-06-21 10:01:26 -0500

    https tenant

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,11 +15,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot() {
         Schema::defaultStringLength(191);
          if (config('tenant.force_https')){
-        //    URL::forceScheme('https');
+           URL::forceScheme('https');
         }
 
         Document::observe(DocumentObserver::class);
-        Carbon::setUTF8(true);
         Carbon::setLocale(config('app.locale'));
         setlocale(LC_ALL, 'es_MX', 'es', 'ES', 'es_MX.utf8');
 
