ab4037e949b8a0decd4aa1f1cc0c296a0efd141c
Darkent <darkent4ever@gmail.com>
2023-07-15 12:28:37 -0500

    log

--- modules/Restaurant/Http/Controllers/TableController.php
+++ modules/Restaurant/Http/Controllers/TableController.php
@@ -80,7 +80,7 @@ class TableController extends Controller
                 foreach ($row as $table) {
                     //buscar las ordenes de la mesa
                     $ordens = Orden::where('table_id', $table->id)->where('status_orden_id', '<>', 4)->where('status_orden_id', '<>', 5)->get();
-                    dump(count($ordens));
+
                     if (count($ordens) == 0) {
                         $table->status_table_id = 1;
                         $table->save();
