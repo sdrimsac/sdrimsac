3aa86ec0fdc4d68f94d592e76dc9b86540ba29d0
Darkent <darkent4ever@gmail.com>
2023-07-20 11:00:41 -0500

    ticket..dos nombres ticket

--- modules/Consignment/Http/Controllers/ConsignmentController.php
+++ modules/Consignment/Http/Controllers/ConsignmentController.php
@@ -117,7 +117,7 @@ class ConsignmentController extends Controller
                 'establishment',
                 'customer'
             ))
-            ->setPaper(array(0, 0, 150, $height));
+            ->setPaper(array(0, 0, 249, $height));
         } catch (Exception $e) {
             return ['m' => $e->getMessage()];
         }
@@ -148,7 +148,7 @@ class ConsignmentController extends Controller
                 'establishment',
                 'customer'
             ))
-            ->setPaper(array(0, 0, 150, $height));
+            ->setPaper(array(0, 0, 249, $height));
         } catch (Exception $e) {
             return ['m' => $e->getMessage()];
         }
