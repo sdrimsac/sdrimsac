56d3f794e9b1ee637e332d3f651f255a18c67f55
Darkent <darkent4ever@gmail.com>
2023-09-21 09:12:58 -0500

    ticket

--- resources/views/tenant/receipt/index.blade.php
+++ resources/views/tenant/receipt/index.blade.php
@@ -55,7 +55,7 @@
         }
 
         .full-width {
-            width: 85%;
+            width: 100%;
         }
 
         .margin-right {
@@ -153,7 +153,7 @@
 </head>
 
 <body>
-    <table border="0" class="full-width">
+    <table border="0" style="width: 90%">
         <tr>
             <td height="80px" valign="top">
                 <table border="0" class="full-width">
