921d24f1c8d7bb47b2c4c98e2c39b17f56cb8a5c
Darkent <darkent4ever@gmail.com>
2023-11-14 09:26:04 -0500

    reporte

--- modules/Format/Http/Controllers/FormatController.php
+++ modules/Format/Http/Controllers/FormatController.php
@@ -121,7 +121,7 @@ class FormatController extends Controller
                                 //->where('establishment_id',auth()->user()->establishment_id)
                                 ->whereIn('document_type_id', ['01', '03','07','08'])
                                 ->whereIn('currency_type_id', ['PEN'])
-                                ->whereIn('soap_type_id', ['01'])
+                                ->whereIn('soap_type_id', ['02'])
                                 ->orderBy('series')
                                 ->orderBy('number')
                                 ->get()->transform(function($row) {
