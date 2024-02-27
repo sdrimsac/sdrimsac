03a7db9c47b1a137389337410db35167af3ecc18
Darkent <darkent4ever@gmail.com>
2024-02-27 07:58:48 -0500

    guia prog.

--- app/Console/Commands/CheckDispatch.php
+++ app/Console/Commands/CheckDispatch.php
@@ -14,21 +14,21 @@ use Symfony\Component\Process\Process;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 use Illuminate\Support\Facades\Storage;
 
-class SendDispatch extends Command
+class CheckDispatch extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'send:dispatches';
+    protected $signature = 'check:dispatches';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Enviar las guias';
+    protected $description = 'Verificar los tickets de las guias de remisión';
 
     /**
      * Create a new command instance.
@@ -47,7 +47,7 @@ class SendDispatch extends Command
      */
     public function handle()
     {
-        
+
         Auth::login(User::firstOrFail());
 
         if (Configuration::firstOrFail()->cron) {
@@ -61,33 +61,37 @@ class SendDispatch extends Command
                 ->select('external_id')
                 ->where([
                     'soap_type_id' => Company::firstOrFail()->active()->soap_type_id,
-                    'state_type_id' => '01'])
+                    'state_type_id' => '03'
+                ])
                 ->whereDate('date_of_issue', '>=', now()->subDays(2))
                 ->get();
             $curl = curl_init();
             foreach ($documents as $document) {
 
                 curl_setopt_array($curl, array(
-                    CURLOPT_URL => "https://{$hostname->fqdn}".'/service/dispatch/send/'.$document->external_id,
+                    CURLOPT_URL => "https://{$hostname->fqdn}" . '/dispatches/status_ticket',
                     CURLOPT_RETURNTRANSFER => true,
                     CURLOPT_ENCODING => '',
                     CURLOPT_MAXREDIRS => 10,
                     CURLOPT_TIMEOUT => 0,
                     CURLOPT_FOLLOWLOCATION => true,
                     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                    CURLOPT_CUSTOMREQUEST => 'GET',
+                    CURLOPT_CUSTOMREQUEST => 'POST',
+                    CURLOPT_POSTFIELDS => '{
+                        "external_id": "' . $document->external_id . '"
+                      }
+                      ',
                     CURLOPT_HTTPHEADER => array(
-                      'Content-Type: application/json',
-                      'Authorization: Bearer '.auth()->user()->api_token
+                        'Content-Type: application/json',
+                        'Authorization: Bearer ' . auth()->user()->api_token
                     ),
-                  ));
+                ));
 
                 $response = curl_exec($curl);
                 $res = json_decode($response, true);
                 if (!$res['success']) $this->info("{$document->date_of_issue} - {$res['message']}");
             }
-        }
-        else {
+        } else {
             $this->info('The crontab is disabled');
         }
         $this->info('ha finalizado la actualización');
