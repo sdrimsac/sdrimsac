0e52f1fef088148f0ef9683f618ea7e1276cbf32
Darkent <darkent4ever@gmail.com>
2024-07-22 12:46:08 -0500

    etiqueta

--- modules/Etiquetas/Http/Controllers/EtiquetasController.php
+++ modules/Etiquetas/Http/Controllers/EtiquetasController.php
@@ -83,7 +83,7 @@ class EtiquetasController extends Controller
             $pdf = new Mpdf([
                 'mode' => 'utf-8',
                 'format' => [
-                    $paper ==  1 ? 50 :  70,
+                    $paper ==  1 ? 50 :  65,
                     $paper == 1 ? 25 : 23
                 ],
 
