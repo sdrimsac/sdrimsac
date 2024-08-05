f8c9effb2ef938b999159ed3265a517e923d48e0
Darkent <darkent4ever@gmail.com>
2024-08-05 16:37:01 -0500

    purchase

--- app/CoreFacturalo/Templates/pdf/trade_name/purchase_a4.blade.php
+++ app/CoreFacturalo/Templates/pdf/trade_name/purchase_a4.blade.php
@@ -151,7 +151,7 @@
                         @if (isset($row->item->color_size))
                             @foreach ($row->item->color_size as $color_size)
                                 <br />{!! '<strong>Color: </strong>' . $color_size->color !!} {!! ' <strong>Talla:</strong> ' . $color_size->size !!} <strong>-
-                                    Cant:</strong> {{ $color_size->quantity }}
+                                    Cant:</strong> {{ $color_size->stock }}
                             @endforeach
                         @endif
                         @if (isset($row->item->lots_group) && count($row->item->lots_group) > 0)
