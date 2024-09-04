0eaba35fba1d7689ca07dc2567a0c3af79dc91b9
Darkent <darkent4ever@gmail.com>
2024-09-04 12:23:55 -0500

    traslados

--- modules/Inventory/Http/Controllers/TransferPlaceController.php
+++ modules/Inventory/Http/Controllers/TransferPlaceController.php
@@ -158,82 +158,63 @@ class TransferPlaceController extends Controller
         }
 
         $result = DB::connection('tenant')->transaction(function () use ($transfer) {
-            $details = TransferPlaceDetail::where('transfers_place_id', $transfer->id)->get();
+            // $details = TransferPlaceDetail::where('transfers_place_id', $transfer->id)->get();
 
-            foreach ($details as $it) {
+            // foreach ($details as $it) {
 
-                $item_orig = ItemWarehouse::where('item_id', $it->item_id)
-                    ->where('warehouse_id', $transfer->warehouse_id)
-                    ->first();
-                if ($item_orig) {
-                    $item_orig->stock += $it->quantity;
-                    $item_orig->save();
-                }
+            //     $item_orig = ItemWarehouse::where('item_id', $it->item_id)
+            //         ->where('warehouse_id', $transfer->warehouse_id)
+            //         ->first();
+            //     if ($item_orig) {
+            //         $item_orig->stock += $it->quantity;
+            //         $item_orig->save();
+            //     }
 
-                /* $item_dest = ItemWarehouse::where('item_id', $it->item_id)
-                    ->where('warehouse_id', $transfer->warehouse_id_destination)
-                    ->first();
-                if ($item_dest) {
-                    $item_dest->stock -= $it->quantity;
-                    $item_dest->save();
-                } */
+            //     $series_lots = $it->series_lots;
+            //     foreach ($series_lots['series'] as $lot) {
+            //         if ($lot['has_sale']) {
+            //             $item_lot = ItemLot::findOrFail($lot['id']);
+            //             $item_lot->warehouse_id = $transfer->warehouse_id;
+            //             $item_lot->update();
+            //         }
+            //     }
 
-                $series_lots = $it->series_lots;
-                foreach ($series_lots['series'] as $lot) {
-                    if ($lot['has_sale']) {
-                        $item_lot = ItemLot::findOrFail($lot['id']);
-                        $item_lot->warehouse_id = $transfer->warehouse_id;
-                        $item_lot->update();
-                    }
-                }
+            //     foreach ($series_lots['lotes'] as $lote) {
+            //         $item_group = ItemLotsGroup::find($lote["id"]);
+            //         $quantity = $lote["quantity"];
+            //         if ($item_group) {
+            //             $item_group->quantity -= $quantity;
+            //             $item_group->save();
 
-                foreach ($series_lots['lotes'] as $lote) {
-                    $item_group = ItemLotsGroup::find($lote["id"]);
-                    $quantity = $lote["quantity"];
-                    if ($item_group) {
-                        $item_group->quantity -= $quantity;
-                        $item_group->save();
+            //             $item_group_orig = ItemLotsGroup::where("item_id", $lote["item_id"])
+            //                 ->where('warehouse_id', $transfer->warehouse_id)
+            //                 ->where('code', $item_group->code)
+            //                 ->first();
 
-                        $item_group_orig = ItemLotsGroup::where("item_id", $lote["item_id"])
-                            ->where('warehouse_id', $transfer->warehouse_id)
-                            ->where('code', $item_group->code)
-                            ->first();
+            //             if ($item_group_orig) {
+            //                 $item_group_orig->quantity += $quantity;
+            //                 $item_group_orig->save();
+            //             }
+            //         }
+            //     }
 
-                        if ($item_group_orig) {
-                            $item_group_orig->quantity += $quantity;
-                            $item_group_orig->save();
-                        }
-                    }
-                }
+            //     foreach ($series_lots['color_size'] as $item_color) {
+            //         $size = $item_color['size'];
+            //         $color = $item_color['color'];
+            //         $quantity = $item_color['selectedQuantity'];
 
-                foreach ($series_lots['color_size'] as $item_color) {
-                    $size = $item_color['size'];
-                    $color = $item_color['color'];
-                    $quantity = $item_color['selectedQuantity'];
+            //         $item_color_size_orig = ItemColorSize::where('warehouse_id', $transfer->warehouse_id)
+            //             ->where('size', $size)
+            //             ->where('color', $color)
+            //             ->where('item_id', $it->item_id)
+            //             ->first();
 
-                    /* $item_color_size_dest = ItemColorSize::where('warehouse_id', $transfer->warehouse_id_destination)
-                        ->where('size', $size)
-                        ->where('color', $color)
-                        ->where('item_id', $it->item_id)
-                        ->first();
-    
-                    if ($item_color_size_dest) {
-                        $item_color_size_dest->stock -= $quantity;
-                        $item_color_size_dest->save();
-                    } */
-
-                    $item_color_size_orig = ItemColorSize::where('warehouse_id', $transfer->warehouse_id)
-                        ->where('size', $size)
-                        ->where('color', $color)
-                        ->where('item_id', $it->item_id)
-                        ->first();
-
-                    if ($item_color_size_orig) {
-                        $item_color_size_orig->stock += $quantity;
-                        $item_color_size_orig->save();
-                    }
-                }
-            }
+            //         if ($item_color_size_orig) {
+            //             $item_color_size_orig->stock += $quantity;
+            //             $item_color_size_orig->save();
+            //         }
+            //     }
+            // }
 
             return [
                 'success' => true,
