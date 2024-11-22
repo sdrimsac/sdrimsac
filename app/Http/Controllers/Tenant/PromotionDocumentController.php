50429e3357903518191c8f96c08eb3b0ea8aae50
Darkent <darkent4ever@gmail.com>
2024-11-22 11:58:43 -0500

    promociones ajuste

--- app/Http/Controllers/Tenant/PromotionDocumentController.php
+++ app/Http/Controllers/Tenant/PromotionDocumentController.php
@@ -194,6 +194,11 @@ class PromotionDocumentController extends Controller
                 ->where('acc_total', $promotion->total)
                 ->where('active', 1)
                 ->count();
+            $count_desactive = PromotionDocumentCustomer::where('promotion_document_id', $row)->where('customer_id', $id)
+                ->where('acc_total', $promotion->total)
+                ->where('active', 0)
+                ->count();
+            $counts = $counts - $count_desactive;
             if ($counts > $limit_changes) {
                 $counts = $limit_changes;
             }
