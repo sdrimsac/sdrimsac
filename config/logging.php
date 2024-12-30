d1aed48c95706be4ffbf44c7a4772bed1794cc28
Darkent <darkent4ever@gmail.com>
2024-12-30 11:23:00 -0500

    log fechas

--- config/logging.php
+++ config/logging.php
@@ -35,7 +35,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['daily'],
         ],
 
         'single' => [
@@ -46,7 +46,7 @@ return [
 
         'daily' => [
             'driver' => 'daily',
-            'path' => storage_path('logs/laravel.log'),
+            'path' => storage_path('logs/laravel-'.date('Y-m-d').'.log'), 
             'level' => 'debug',
             'days' => 7,
         ],
