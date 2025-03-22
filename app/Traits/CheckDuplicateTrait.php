2fa0943792feaa7ce8b9857fb8a2adc93075b414
sdrDevelop <sdrimsacteams@gmail.com>
2025-03-22 15:03:33 -0500

    ver si valida los mensajes

--- app/Traits/CheckDuplicateTrait.php
+++ app/Traits/CheckDuplicateTrait.php
@@ -4,6 +4,7 @@ namespace App\Traits;
 
 use App\Http\Controllers\Tenant\WhatsappController;
 use App\Models\Tenant\Catalogs\DocumentType;
+use App\Models\Tenant\Document;
 use App\Models\Tenant\Establishment;
 use Carbon\Carbon;
 
@@ -14,10 +15,10 @@ trait CheckDuplicateTrait
 {
     /**
      * Get type doc
-     * @param  string $documentType
+     * @param  \App\Models\Tenant\Document $document  
      * @return int
      */
-    public function checkDuplicateAndSendMessage($document) {
+    /* public function checkDuplicateAndSendMessage($document) {
         $documentTypeDescription = $document->document_type->description;
         $serie = $document->series;
         $number = $document->number;
@@ -25,5 +26,25 @@ trait CheckDuplicateTrait
 
         $message = "El documento {$documentTypeDescription} {$serie}-{$number} de fecha {$date_of_issue} se ha duplicado.";
         (new WhatsappController)->sendMessageAllSupprotDuplicate($message);
+    } */
+    public function checkDuplicateAndSendMessage($document)
+    {
+        $documentTypeDescription = $document->document_type->description;
+        $serie = $document->series;
+        $number = $document->number;
+        $total = $document->total;
+        $date_of_issue = Carbon::parse($document->date_of_issue)->format('d/m/Y');
+
+        // Buscar si ya existe otro documento con los mismos valores
+        $exists = Document::where('series', $serie)
+            ->where('number', $number)
+            ->where('total', $total)
+            ->where('id', '!=', $document->id) // Excluir el mismo documento si es una actualización
+            ->exists();
+
+        if ($exists) {
+            $message = "⚠️ El documento {$documentTypeDescription} {$serie}-{$number} de fecha {$date_of_issue} se ha duplicado con el mismo total ({$total}).  mensaje de pruebas";
+            (new WhatsappController)->sendMessageAllSupprotDuplicate($message);
+        }
     }
 }
