4d2290c14598c2f4af3331e8ac87d181dae85cfc
sdrDevelop <sdrimsacteams@gmail.com>
2025-04-17 19:20:25 -0500

    correcion x

--- resources/js/views/configurations/form.vue
+++ resources/js/views/configurations/form.vue
@@ -6398,6 +6398,62 @@
                                                                             </div>
                                                                         </div>
                                                                     </div>
+                                                                    <div
+                                                                        class="col-md-3 mt-4"
+                                                                    >
+                                                                        <div
+                                                                            class="card h-100"
+                                                                        >
+                                                                            <div
+                                                                                class="card-body d-flex flex-column"
+                                                                            >
+                                                                                <div
+                                                                                    class="form-group text-left flex-grow-1"
+                                                                                >
+                                                                                    <label
+                                                                                        class="control-label w-100"
+                                                                                    >
+                                                                                        <i
+                                                                                            class="el-icon-s-grid icon-tab"
+                                                                                        ></i>
+                                                                                        Reporte de yape en caja 
+                                                                                        <el-tooltip
+                                                                                            class="item"
+                                                                                            effect="dark"
+                                                                                            content="Al activar este boton se mostrara el reporte de yape en histroial para cada cierre de caja y tambien al cerrar la caja va enviar el reporte de caja y el de yape al wasap"
+                                                                                            placement="top-start"
+                                                                                        >
+                                                                                            <i
+                                                                                                class="fa fa-info-circle"
+                                                                                            ></i>
+                                                                                        </el-tooltip>
+                                                                                    </label>
+                                                                                    <el-switch
+                                                                                        v-model="
+                                                                                            form.yape_report
+                                                                                        "
+                                                                                        active-text="Si"
+                                                                                        inactive-text="No"
+                                                                                        :active-color="
+                                                                                            '#28a745'
+                                                                                        "
+                                                                                        :inactive-color="
+                                                                                            '#6c757d'
+                                                                                        "
+                                                                                        @change="
+                                                                                            () =>
+                                                                                                submit(
+                                                                                                    'reporte de yape en caja',
+                                                                                                    form.yape_report,
+                                                                                                    'yape_report',
+                                                                                                    'lockIcon'
+                                                                                                )
+                                                                                        "
+                                                                                    ></el-switch>
+                                                                                </div>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
                                                                 </div>
                                                             </div>
                                                         </div>
@@ -13989,82 +14045,7 @@
                                                                 </div>
                                                             </div>
                                                         </div>
-                                                        <div
-                                                            class="col-md-3 mt-4"
-                                                        >
-                                                            <div
-                                                                class="card h-100"
-                                                            >
-                                                                <div
-                                                                    class="card-body d-flex flex-column"
-                                                                >
-                                                                    <div
-                                                                        class="form-group text-left flex-grow-1"
-                                                                    >
-                                                                        <label
-                                                                            class="control-label w-100"
-                                                                            :style="{
-                                                                                color: form.variation_hotel
-                                                                                    ? '#28a745'
-                                                                                    : '#6c757d'
-                                                                            }"
-                                                                        >
-                                                                            <i
-                                                                                ref="variationIcon"
-                                                                                class="fas fa-boxes tab-icon"
-                                                                            ></i>
-                                                                            Mostrar reporte de yape y Enviar al Wasap
-                                                                            <el-tooltip
-                                                                                class="item"
-                                                                                effect="dark"
-                                                                                content="al activar este boton en historial se visualizara la opcion de ver el reporte de yapes por cada caja asi como tambien al cerrar la caja se enviara el pdf al wasap del dueño del negocio"
-                                                                                placement="top-start"
-                                                                            >
-                                                                                <i
-                                                                                    class="fa fa-info-circle"
-                                                                                ></i>
-                                                                            </el-tooltip>
-                                                                        </label>
-                                                                        <el-select
-                                                                            :disabled="
-                                                                                !form.variation_hotel
-                                                                            "
-                                                                            ref="producto"
-                                                                            v-model="
-                                                                                form.item_variation_id
-                                                                            "
-                                                                            @change="
-                                                                                changeItem
-                                                                            "
-                                                                            filterable
-                                                                            remote
-                                                                            placeholder="Buscar..."
-                                                                            popper-class="el-select-items"
-                                                                            :remote-method="
-                                                                                searchRemoteItems
-                                                                            "
-                                                                            :loading="
-                                                                                loading_search
-                                                                            "
-                                                                        >
-                                                                            <el-option
-                                                                                v-for="option in items"
-                                                                                :key="
-                                                                                    option.id
-                                                                                "
-                                                                                :value="
-                                                                                    option.id
-                                                                                "
-                                                                                :label="
-                                                                                    option.full_description ||
-                                                                                        option.description
-                                                                                "
-                                                                            ></el-option>
-                                                                        </el-select>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                                                        </div>
+                                                        
                                                         <!-- <div class="col-md-2 mt-4">
                                                         <div class="form-group">
                                                             <label
