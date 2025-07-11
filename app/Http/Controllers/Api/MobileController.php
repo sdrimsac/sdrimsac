5fc569a2652a8400d914e7504af922a51cd66bc9
sdrDevelop <sdrimsacteams@gmail.com>
2025-07-11 18:21:31 -0500

    item_warehouse en productos

--- app/Http/Controllers/Api/MobileController.php
+++ app/Http/Controllers/Api/MobileController.php
@@ -138,6 +138,14 @@ class MobileController extends Controller
                             'is_set' => (bool) $row->is_set,
                             'aux_quantity' => 1,
                             'image_url' => $row->image_url,
+                            'warehouse' => $row->item_warehouse->map(function($warehouse) {
+                                return [
+                                    'id' => $warehouse->id,
+                                    'description' => $warehouse->description,
+                                    'quantity' => $warehouse->quantity,
+                                    'stock' => $warehouse->stock
+                                ];
+                            })->toArray()
                         ];
                     });
 
