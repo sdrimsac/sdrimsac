14856ad4d93f9d691f237d393777605c31aa0b61
sdrimsac <sdrimsac@gmail.com>
2025-10-21 13:06:12 -0500

    correcion

--- app/CoreFacturalo/Requests/Inputs/DocumentInput.php
+++ app/CoreFacturalo/Requests/Inputs/DocumentInput.php
@@ -319,7 +319,7 @@ class DocumentInput
                         'internal_id' => $item->internal_id,
                         'item_code' => trim($item->item_code),
                         'item_code_gs1' => $item->item_code_gs1,
-                        'unit_type_id' => (key_exists('item', $row)) ? $row['item']['unit_type_id'] : $item->unit_type_id,
+                        'unit_type_id' => (key_exists('item', $row)) ? Functions::valueKeyInArray($row['item'], 'unit_type_id', $item->unit_type_id) : $item->unit_type_id,
                         'presentation' => (key_exists('item', $row)) ? (isset($row['item']['presentation']) ? $row['item']['presentation'] : []) : [],
                         'amount_plastic_bag_taxes' => $item->amount_plastic_bag_taxes,
                         'is_set' => $item->is_set,
